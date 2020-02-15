d3ed030f5d729a9ad3ab3e4a31bc55b815d4ee87
伸一 石割 <ishiwari.masuoka@gmail.com>
2020-02-15 11:59:42 +0900

    "Now branch is develop!"　を再追加　全部で２つ

--- git-test/Source.cpp
+++ git-test/Source.cpp
@@ -4,6 +4,7 @@ int main() {
 	std::cout << "Hello World" << std::endl;
 	std::cout << "Let's Commit!" << std::endl;
 	std::cout << "Now branch is develop!" << std::endl;
+	std::cout << "Now branch is develop!" << std::endl;
 	return 0;
 }
 //   add
