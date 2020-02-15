3bae370a091b8b475383c22e88168cefc969bf86
伸一 石割 <ishiwari.masuoka@gmail.com>
2020-02-15 11:34:25 +0900

    "Now branch is develop!"を書き込みました。

--- git-test/Source.cpp
+++ git-test/Source.cpp
@@ -3,6 +3,6 @@
 int main() {
 	std::cout << "Hello World" << std::endl;
 	std::cout << "Let's Commit!" << std::endl;
-	std::cout << "Now branch is master!" << std::endl;
+	std::cout << "Now branch is develop!" << std::endl;
 	return 0;
 }
